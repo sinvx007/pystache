9079d5377030a8e32270bd9e1c27e8360de88b65
Rory Geoghegan <r.geoghegan@gmail.com>
2012-11-17 19:02:59 -0500

    Fix typo in script

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -3,5 +3,5 @@
 . ~/.virtualenvs/python2.7/bin/activate
 
 PYTHONPATH=. python -m coverage run pystache/commands/test.py
-python -m coverage -o coverage.xml
+python -m coverage xml -o coverage.xml
 

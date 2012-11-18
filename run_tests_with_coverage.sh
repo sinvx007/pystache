2485bbbf7441aed15c626ab61e419849f2ba823c
Rory Geoghegan <r.geoghegan@gmail.com>
2012-11-17 19:11:05 -0500

    Have html coverage reports

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -3,5 +3,7 @@
 . ~/.virtualenvs/python2.7/bin/activate
 
 PYTHONPATH=. python -m coverage run pystache/commands/test.py
+
 python -m coverage xml -o coverage.xml
+python -m coverage html -d coverage
 

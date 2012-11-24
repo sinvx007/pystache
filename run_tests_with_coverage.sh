85569375f77e16dbc627784aa83c6de93b92bf66
Rory Geoghegan <r.geoghegan@gmail.com>
2012-11-24 12:15:21 -0500

    Have script with coverage

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -2,8 +2,12 @@
 
 . ~/.virtualenvs/python2.7/bin/activate
 
+rm -f pep8.log pyflakes.log
+
 PYTHONPATH=. python -m coverage run pystache/commands/test.py
 
 python -m coverage xml -o coverage.xml
 python -m coverage html -d coverage
 
+pep8 --max-line-length=120 pystache > pep8.log
+pyflakes pystache > pyflakes.log

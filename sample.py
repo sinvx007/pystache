cce9728e396d1ea3bb3e4499000d2c28b9191ed9
Vikas <Vikas@xyz.com>
2018-05-11 09:34:46 +0000

    created a py func

---
+++ sample.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+number = input("Enter an integer number--> ")
+
+def Even(number):
+    if number % 2 == 0:
+        print ("Its an Even number")
+    else:
+        print("Its a Odd number")
+Even(number)

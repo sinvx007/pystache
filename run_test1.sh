e303fc4c6a7701b42d88ff11ac454e6903561df7
Vikas <Vikas@xyz.com>
2018-05-13 17:24:15 +0000

    Added test

---
+++ run_test1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f /home/ec2-user/mygitdir/test.py ]
+then
+echo "Test is successfully"
+else
+echo "Test failed"
+fi
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e 
-
+echo "This is test"
 . ~/.virtualenvs/python2.7/bin/activate
 
 PYTHONPATH=. python -m pystache.commands.test
